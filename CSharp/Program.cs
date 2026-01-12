53ba532011c3fa34cef4a76206ce9c18d2d170c2
kamto <107313631+kamto-s@users.noreply.github.com>
2026-01-12 17:00:33 +0700

    Initial commit ASP.NET project

---
+++ CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Im learning c#");
+            Console.WriteLine("It is awesome");
+            Console.WriteLine(3 + 3);
+        }
+    }
+}
