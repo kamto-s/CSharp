9704922d81f376f8a12ad13af8aa5010798965d8
kamto <107313631+kamto-s@users.noreply.github.com>
2026-01-13 11:04:24 +0700

    variable

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,8 +7,22 @@ namespace CSharp
         static void Main(string[] args)
         {
             string name = "Jhonny";
-
             Console.WriteLine(name);
+
+            int myNum = 15;
+            myNum = 20;
+            Console.WriteLine(myNum);
+
+            double myDouble = 19.99;
+            Console.WriteLine(myDouble);
+
+            char myChar = 'D';
+            Console.WriteLine(myChar);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
