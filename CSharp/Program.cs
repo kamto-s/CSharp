9ce494c52fff7e96930b6bb449a5ce8b46ff6771
kamto <107313631+kamto-s@users.noreply.github.com>
2026-01-13 11:22:03 +0700

    variable

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,6 +6,7 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("======================= VARIABLES ========================");
             string name = "Jhonny";
             Console.WriteLine(name);
 
@@ -23,6 +24,28 @@ namespace CSharp
             Console.WriteLine(myBool);
 
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("======================= VARIABLES CONST ========================");
+            const int myConstNum = 10;
+            Console.WriteLine(myConstNum);
+
+            Console.WriteLine("======================= VARIABLES DISPLAY ========================");
+            string firstName = "John";
+            string lastName = "Doe";
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            int x = 5;
+            int y = 4;
+            int z = x + y;
+            Console.WriteLine(z);
+
+            Console.WriteLine("========================= Multiple Variables ======================");
+            int a, b, c;
+            a = b = c = 50;
+            Console.WriteLine(a + b + c);
+
+
         }
     }
 }
