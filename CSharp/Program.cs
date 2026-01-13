06a3a017295010630bbbd64f1769aa3d1005ec3b
kamto <107313631+kamto-s@users.noreply.github.com>
2026-01-13 12:33:27 +0700

    user input

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,30 +6,18 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("======================= TYPE CASTING ========================");
-            int myInt = 10;
-            double myDouble = myInt;
+            Console.WriteLine("======================= User Input ========================");
+           
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
 
-            Console.WriteLine(myInt);
-            Console.WriteLine(myDouble);
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("===============================================");
-
-            double myDouble2 = 9.78;
-            int myInt2 = (int)myDouble2;
-
-            Console.WriteLine(myDouble2);
-            Console.WriteLine(myInt2);
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Your age is " + age);
 
             Console.WriteLine("===============================================");
-            int myInt3 = 9;
-            double myDouble3 = 4.25;
-            bool myBool = false;
-
-            Console.WriteLine(Convert.ToString(myInt3));
-            Console.WriteLine(Convert.ToDouble(myInt3));
-            Console.WriteLine(Convert.ToInt32(myDouble3));
-            Console.WriteLine(Convert.ToString(myBool));
         }
     }
 }
