cf3318a36b8b7d0946189d9e674799dcdc10fcd1
kamto <107313631+kamto-s@users.noreply.github.com>
2026-01-22 23:52:53 +0700

    commit

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿int k;
+int n = 10;
+string kota = "Jakarta";
+float total = 2.56f;
+double pi = 3.14d;
+int bil1 = 100;
+float bil2 = 25.55f;
+string pesan = "Selamat datang di pemrograman C#";
 
-namespace CSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Im learning c#");
-            Console.WriteLine("It is awesome");
-            Console.WriteLine(3 + 3);
-        }
-    }
-}
+k = 100;
+
+Console.WriteLine("=========================================================");
+Console.WriteLine(bil1);
+Console.WriteLine("bil1:{0}", bil1);
+Console.WriteLine("bil1: {0}, bil2: {1}, pesan: {2}", bil1, bil2, pesan);
+Console.WriteLine($"bil1: {bil1}, bil2: {bil2}, pesan: {pesan}");
+Console.WriteLine();
+
+Console.WriteLine("=========================================================");
+Console.WriteLine("Ini hari jum'at");
+Console.WriteLine("Ingat tanda \"*\" menunjukan kali");
+Console.WriteLine();
+
+Console.WriteLine("=========================================================");
+Console.Write("*");
+Console.Write("*");
+Console.Write("*");
+Console.Write("*");
+Console.WriteLine("*");
+Console.WriteLine("*****");
+Console.WriteLine();
+
+
+Console.WriteLine("=========================================================");
+Console.WriteLine("n = {0}", n);
+Console.WriteLine("kota = {0}", kota);
+Console.WriteLine("total = {0}", total);
+Console.WriteLine("pi = {0}", pi);
+Console.WriteLine("k = {0}", k);
+Console.WriteLine();
+
+Console.WriteLine("=========================================================");
